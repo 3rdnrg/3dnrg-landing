7861ccba95bc2b78057d5af919b863feb3684773
stephane955 <75364548+stephane955@users.noreply.github.com>
2024-06-26 12:21:52 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
                         <div className="sm:flex sm:items-center sm:justify-between">
                             <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse" aria-label="Homepage">
                                 <img src="/logo.JPG" className="h-8" alt="Flowbite Logo"/>
-                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">3DNRG</span>
+                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
                             </Link>
                             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                                 <li>
@@ -34,7 +34,7 @@ const Footer = () => {
                             </ul>
                         </div>
                         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
-                        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">3DNRG™</Link>. All Rights Reserved.</span>
+                        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">3DNRG™</Link>. All Rights Reserved.</span>
                     </div>
                 </footer>
             </div>
